b73a4bc99b0171e18349ea4eeb83637d3f445c36
BIOSYSTEM <46874046+lilbios@users.noreply.github.com>
2020-04-22 17:14:39 +0300

    implemented di approach

--- bioResearchSystem/bioResearchSystem.API/Startup.cs
+++ bioResearchSystem/bioResearchSystem.API/Startup.cs
@@ -2,10 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using bioResearchSystem.Context;
+using bioResearchSystem.DAL;
+using bioResearchSystem.DAL.Implementations;
+using bioResearchSystem.DAL.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +30,23 @@ namespace bioResearchSystem.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            //Used for connection for database
+            var connection = Configuration.GetConnectionString("DefaultConnection");
             services.AddControllers();
+            services.AddDbContext<BioResearchSystemDbContex>(options => options.UseSqlServer(connection,
+                assembly => assembly.MigrationsAssembly(nameof(Context))));
+
+            //DI approach
+            services.AddTransient<IRepositoryDevice,DeviceRepository>();
+            services.AddTransient<IRepositoryExperiment, ExperimentRepository>();
+            services.AddTransient<IRepositoryObjective, ObjectiveRepository>();
+            services.AddTransient<IRepositoryResearch, ResearchRepository>();
+            services.AddTransient<IRepositoryResult, ResultRepository>();
+            services.AddTransient<IRepositoryTopic, TopicRepository>();
+            services.AddTransient<IRepositoryUser, UserRepository>();
+            services.AddScoped<DataManager>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
