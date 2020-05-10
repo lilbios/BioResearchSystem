5df804cc3a1fb34a75355a41cc8784a1b03fc25b
BIOSYSTEM <46874046+lilbios@users.noreply.github.com>
2020-05-11 00:17:41 +0300

    fixed conflicts

---
+++ bioResearchSystem/bioResearchSystem.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bioResearchSystem.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
